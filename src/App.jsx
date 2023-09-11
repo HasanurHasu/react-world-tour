03aa8af4cfc9b8a7172c27b1322958c0896ec759
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-09-11 16:09:40 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Countries from './Components/Countries/Countries'
 function App() {
   return (
     <>
-      <h1 className="text-2xl text-center font-bold">React World Tour</h1>
+      <h1 className="text-2xl text-center font-bold">World Tour</h1>
       <Countries></Countries>
     </>
   )
